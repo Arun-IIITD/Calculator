b0f69ac7cece2b151aa7063d0b71fa747d3151f1
Arun Yadav <ay20552291@gmail.com>
2025-09-17 22:42:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [input,setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  // const handleclick = (value) => {
+  //   setInput("")
+  //   setResult("")
+  // }
+
+  const handleclick = (value) => {
+    if (value === "C") {
+      setInput("");
+      setResult("");
+    } 
+
+    else if (value === "="){
+      calculate()
+    }
+
+    else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+  const calculate = () => {
+    try{
+      const evaluated_res = eval(input);
+
+      if (evaluated_res === Infinity){
+        setResult("Infinity")
+      }
+      else if (isNaN(evaluated_res)){
+        setResult("Nan")
+      }
+      else{
+        setResult(evaluated_res)
+      }
+
+    }catch(error){
+      setResult(error);
+    }
+
+  }
+
+   const buttons = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", "C", "=", "+"
+  ];
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>React Calculator</h1>
+      <form>
+        <input
+        type = "text"
+        placeholder="0"
+        value={input}
+        //onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+
+      <div className="buttons">
+        {buttons.map((btn) => (
+          <button key ={btn} onClick={() => handleclick(btn)}>
+            {btn}
+          </button>
+        ))}
+ 
+
+
+
+      </div>
+
+      <div className="result">
+         {result}
+      </div>
+   
     </div>
   );
 }
